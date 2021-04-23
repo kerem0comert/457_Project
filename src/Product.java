ea7f73a2f1a203ba7c3073a2f51c65fac35ba0ad
forumber <kidobeko@gmail.com>
2021-04-23 21:40:05 +0300

    Product: Add fields

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,14 @@
+import java.util.List;
+
 public class Product {
+    protected String id;
+    protected String name;
+    protected String price;
+    protected String brand;
+    protected String screenSize;
+    protected String model;
+    protected String productBehavior;
+    protected String extraFeatures;
+    protected List<String> ExtraFeatures;
+    protected boolean isLargeStorage;
 }
